ca2fce8f25a7683894212e86c1cf69447deb2acd
Alejandro Bajauli <alejandrobajauli@gmail.com>
2024-05-08 01:12:43 +0200

    modified:   INSTRUCTIONS.md 	new file:   LICENSE 	modified:   README.md 	new file:   examples/exercise/input-json/bar.json 	modified:   examples/exercise/input-json/procurement.json 	deleted:    examples/exercise/input-json/www.json 	modified:   examples/exercise/main.tf 	modified:   examples/exercise/variables.tf 	modified:   main.tf 	modified:   outputs.tf 	new file:   terraform-docs 	modified:   variables.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
-/**
-* # Terraform
-*
+# Terraform
 
 # ---------------------------------------------------------------------------------------------------------------------
 
-* # Update of the module description:
+# Module description before the optimization
 
-* # New optimization for the module to dinamically obtain from a .json imported file the A type DNS reg. 
-* # This way you can add multiple DNS entries on the .JSON format. 
+# This tf module imports the defined tf version and initializes the dns provider from hashicorp.
+# It is being used to setup a DNS on zone example.com that resolves around the A type regitries imputed manually.
 
 # ---------------------------------------------------------------------------------------------------------------------
+
+# Update of the module description:
+
+# New optimization for the module to dinamically obtain from a .json imported file the A type DNS reg. 
+# This way you can add multiple DNS entries on the .JSON format. 
 
 # ---------------------------------------------------------------------------------------------------------------------
 # SET TERRAFORM RUNTIME REQUIREMENTS
@@ -33,6 +36,10 @@ terraform {
 
 locals {
 
+  unfiltered = {for x in fileset("${path.module}/${var.pathJson}", "*.json") : trimsuffix(x, ".json") => jsondecode(file("${path.module}/${var.pathJson}/${x}"))}
+  filtered = {for k, v in local.unfiltered : k => v if length([for key in ["ttl", "zone", "dns_record_type"] : key if contains(keys(v), key)]) == 3}
+  records = {for x in ["a", "aaaa", "cname"] : x => {for k, v in local.filtered : k => v if v.dns_record_type == x}}
+  
 }
 
 
@@ -40,13 +47,26 @@ locals {
 # Write your Terraform resources here
 # ------------------------------------------
 
-resource "dns_a_record_set" "www" {
-  zone = "example.com."
-  name = "www"
-  addresses = [
-    "192.168.0.1",
-    "192.168.0.2",
-    "192.168.0.3",
-  ]
-  ttl = 300
+resource "dns_a_record_set" "dns-set-a" {
+  for_each = local.records.a
+  zone = each.value.zone
+  name = each.key
+  addresses = each.value.addresses
+  ttl = each.value.ttl
+}
+
+resource "dns_aaaa_record_set" "dns-set-aaaa" {
+  for_each = local.records.aaaa
+  zone = each.value.zone
+  name = each.key
+  addresses = each.value.addresses
+  ttl = each.value.ttl
+}
+
+resource "dns_cname_record" "dns-set-aaaa" {
+  for_each = local.records.cname
+  zone = each.value.zone
+  name = each.key
+  cname  = each.value.cname
+  ttl = each.value.ttl
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
 # ------------------------------------------
 # Write your Terraform variable inputs here
 # ------------------------------------------
+
+variable "pathJson" {
+  description = "Path to the JSON files containing A type reg"
+  type        = string
+  default     = "/examples/exercise/input-json/"
+}
