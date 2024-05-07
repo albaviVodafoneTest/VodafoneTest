24216f77e10a53470a761f9fddb9ea5c6123b128
Alejandro Bajauli <alejandrobajauli@gmail.com>
2024-05-07 22:42:13 +0200

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,22 @@
 /**
 * # Terraform
 *
-* <Short TF module description>
-*
-*
-* ## Usage
-*
-* ### Quick Example
-*
-* ```hcl
-* module "dns_" {
-*   source = ""
-*   input1 = <>
-*   input2 = <>
-* } 
-* ```
-*
-*/
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ---------------------------------------------------------------------------------------------------------------------
+
+* # Update of the module description:
+
+* # New optimization for the module to dinamically obtain from a .json imported file the A type DNS reg. 
+* # This way you can add multiple DNS entries on the .JSON format. 
+
+# ---------------------------------------------------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------------------------------------------------
 # SET TERRAFORM RUNTIME REQUIREMENTS
 # ---------------------------------------------------------------------------------------------------------------------
 
 terraform {
-  # This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
-  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
-  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
-  # forwards compatible with 0.13.x code.
+
   required_version = ">= 0.13.5"
   required_providers {
     dns = {
